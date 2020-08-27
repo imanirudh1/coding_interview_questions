7478b915c3a8cea637aadc872e8adfa18975f645
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-08-27 19:46:30 +0530

    Find Product

--- find_product.py
+++ find_product.py
@@ -1,9 +1,7 @@
 n=int(input())
 z=[]
-for i in range(0,n):
-    a=int(input())
-    z.append(a)
+z=list(map(int,input().split()))
 answer = z[0]
 for i in z[1:]:
     answer = (answer*i)%(10**9 + 7)
-    print(answer)
+print(answer)
