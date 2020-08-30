164df98a721ef9cd371c0139172bccc14869ea7b
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-08-30 21:36:02 +0530

    String Compression

--- string_compression.py
+++ string_compression.py
@@ -3,13 +3,14 @@ def str_comp(a):
     counter=1
     i=1
     r=''
-    for i in range(1,len(a)):
+    while i<len(a):
         if a[i]==a[i-1]:
             counter+=1
         else:
             r=r+a[i-1]+str(counter)
             counter=1
-    r=r+a[len(a)-1]+str(counter)    
+        i+=1
+    r=r+a[i-1]+str(counter)    
     return r
 
 print(str_comp(a))               
