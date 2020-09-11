609facd691edac091ac348bb6c72ab3aef161ea7
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-11 11:21:50 +0530

    Sequential Search

--- sequential_search.py
+++ sequential_search.py
@@ -13,16 +13,16 @@ def seq_search(arr,ele):
 print(seq_search(arr,6))
 # input array must be sorted
 
-def seq_search(arr,ele):
-    pos=0
-    found=False
-    stopped=False
-    while pos < len(arr) and not found and not stopped:
-        if arr[pos] == ele:
-            return True
-        else:
-            if arr[pos]>ele:
-                stopped=True
-            else:
-                pos += 1
-    return found        
+# def seq_search(arr,ele):
+#     pos=0
+#     found=False
+#     stopped=False
+#     while pos < len(arr) and not found and not stopped:
+#         if arr[pos] == ele:
+#             return True
+#         else:
+#             if arr[pos]>ele:
+#                 stopped=True
+#             else:
+#                 pos += 1
+#     return found        
