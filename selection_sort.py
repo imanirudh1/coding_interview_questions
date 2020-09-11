078abe33001224baaae0184d1e44e6a0698b52f7
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-11 21:41:58 +0530

    Selection Sort

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+def selec_sort(arr):
+    for i in range(0,len(arr)-1):
+        min=i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min]:
+                min=j
+        arr[i],arr[min]=arr[min],arr[i]
+    return arr            
+
+arr=[5,3,2,1,0]
+print(selec_sort(arr))
