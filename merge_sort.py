27a437f122d22d998a04f7b7a5630fdbb07feafa
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-12 20:24:02 +0530

    Merge Sort

--- merge_sort.py
+++ merge_sort.py
@@ -27,7 +27,31 @@ def merge_sort(arr):
             arr[k]=righthalf[j]
             j+=1
             k+=1
+    print('Merging ',arr)        
 
 arr = [11,2,5,4,7,6,8,1,23]
 merge_sort(arr)
 print(arr)
+
+'''
+Output:
+
+Merging  [11]
+Merging  [2]
+Merging  [2, 11]
+Merging  [5]
+Merging  [4]
+Merging  [4, 5]
+Merging  [2, 4, 5, 11]
+Merging  [7]
+Merging  [6]
+Merging  [6, 7]
+Merging  [8]
+Merging  [1]
+Merging  [23]
+Merging  [1, 23]
+Merging  [1, 8, 23]
+Merging  [1, 6, 7, 8, 23]
+Merging  [1, 2, 4, 5, 6, 7, 8, 11, 23]
+[1, 2, 4, 5, 6, 7, 8, 11, 23]
+'''
