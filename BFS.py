963be52173beb62d3b745068e6560f84e60d3152
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-14 23:15:33 +0530

    BFS

---
+++ BFS.py
@@ -0,0 +1,36 @@
+from queue import Queue
+adj_list={
+    'A':['B','D'],
+    'B':['A','C'],
+    'C':['B'],
+    'D':['A','E','F'],
+    'E':['D','F','G'],
+    'F':['D','E','H'],
+    'G':['E','H'],
+    'H':['G','F']
+}
+
+visited={}
+level={}
+parent={}
+bfs_travel_output=[]
+queue=Queue()
+for node in adj_list.keys():
+    visited[node]=False
+    parent[node]=None
+    level[node]=-1
+s='A'
+visited[s]=True
+level[s]=0
+queue.put(s)
+while not queue.empty():
+    u=queue.get()
+    bfs_travel_output.append(u)
+    for v in adj_list[u]:
+        if not visited[v]:
+            visited[v]=True
+            parent[v]=u
+            level[v]=level[u]+1
+            queue.put(v)
+
+print(bfs_travel_output)
