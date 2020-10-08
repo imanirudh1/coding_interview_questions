83f0fa09cbc72184e5af3087ba1036753956ddff
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-08 16:36:01 +0530

    Longest Palindrome

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -6,11 +6,11 @@ l = 0
 for n in range(1, len(s)):
     if n - length >= 1:
         substring = s[n - length - 1 : n + 1]
-        
         if substring == substring[::-1]:
             print('subst 1 ',substring)
             l = n - length - 1
             length += 2
+            print('len1 ',length)
             continue
     if n - length >= 0:
         substring = s[n - length : n + 1]
@@ -19,4 +19,5 @@ for n in range(1, len(s)):
             print('subst 2 ',substring)
             l = n - length
             length += 1
+            print('len2 ',length)
 print(s[l:l+length])
