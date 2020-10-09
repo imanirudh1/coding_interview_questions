6b5b7a197ae21759de7b1e9743872eb0e2bce873
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-09 17:28:24 +0530

    Find Repeating No and missing no

---
+++ find_repeating_and_missing.py
@@ -0,0 +1,34 @@
+x = 0
+y=0
+def getTwoElements(arr, n): 
+    global x, y 
+    x = 0
+    y = 0  
+    xor1 = arr[0] 
+    for i in range(1, n): 
+        xor1 = xor1 ^ arr[i] 
+    for i in range(1, n + 1): 
+        xor1 = xor1 ^ i       
+    print('Xor1 ',xor1)
+    # set_bit_no = xor1 & ~(xor1 - 1) 
+    set_bit_no = xor1 
+    print('Set bit no ',set_bit_no)  
+    for i in range(n): 
+        if (arr[i] & set_bit_no) != 0: 
+            x = x ^ arr[i]
+            print('X : ',x)
+        else: 
+            y = y ^ arr[i] 
+            print('Y : ',y)  
+    for i in range(1, n + 1): 
+        if (i & set_bit_no) != 0: 
+            x = x ^ i 
+        else: 
+            y = y ^ i  
+arr = [ 4,3,6,2,1,1] 
+n = len(arr) 
+      
+getTwoElements(arr, n) 
+  
+print("The missing element is", x, 
+      "and the repeating number is", y) 
