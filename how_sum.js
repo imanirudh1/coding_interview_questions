5cf0c52e6650c5f787a8dbcfda130a85c221cda3
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2020-12-08 19:12:57 +0530

    Create how_sum.js

---
+++ how_sum.js
@@ -0,0 +1,15 @@
+const how_sum=(target,nums,memo={})=>{
+if(memo[target] in memo) return memo[target]
+if(target==0) return []
+if(target<0) return null
+for(let num of nums){
+var remaining= target- num
+var remainingResult=how_sum(remaining,nums, memo)
+if(remainingResult!=null){
+memo[target]=[... remainingResult,num]
+return memo[target]
+}
+}
+memo[target]=null
+return memo [target]
+}
