f744335586cf4b39d670e9378c4a05d023ec8929
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-09 22:00:42 +0530

    Grid Traveler

--- grid_traverse.js
+++ grid_traverse.js
@@ -1,9 +1,10 @@
-const grid_traversal=(m,n,memo={})=>{
-var key = m+","+n
-if(key in memo) return memo[key]
-if(m==0 || n==0) return 0
-if(m>0 && n >0) return 1
-memo[key]=grid_traversal(m-1,n,memo)+grid_traversal(m,n-1,memo)
-return memo[key]
-
+const grid_traversal = (m, n, memo = {}) => {
+  let key = m + ',' + n
+  if (key in memo) return memo[key]
+  if (m === 0 || n === 0) return 0
+  if (m === 1 && n === 1) return 1
+  memo[key] = grid_traversal(m - 1, n, memo) + grid_traversal(m, n - 1, memo)
+  return memo[key]
 }
+
+console.log(grid_traversal(2, 3))
