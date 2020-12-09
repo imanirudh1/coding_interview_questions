d840fd92f6cb234a9d4d248a393f9405578a2b6f
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-09 22:05:33 +0530

    Can Sum

--- can_sum.js
+++ can_sum.js
@@ -1,14 +1,16 @@
-const cansum=(target,nums,memo={})=>{
-if(target in memo) return memo[target]
-If(target <0) return false
-if(target==0) return true
-for(let num of nums){
-var remaining= target-num
-if(cansum(remaining, nums)===true){
-memo [target]=true
-return true
+const cansum = (target, nums, memo = {}) => {
+  if (target in memo) return memo[target]
+  if (target < 0) return false
+  if (target === 0) return true
+  for (let num of nums) {
+    var remaining = target - num
+    if (cansum(remaining, nums) === true) {
+      memo[target] = true
+      return true
+    }
+  }
+  memo[target] = false
+  return false
 }
-}
-memo[target]=false
-return false
-}
+
+console.log(cansum(7, [2, 3]))
