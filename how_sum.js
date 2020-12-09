cd644da6c5b3666715f9857f0478d5af2ad9c3e0
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-09 22:10:26 +0530

    How Sum

--- how_sum.js
+++ how_sum.js
@@ -1,15 +1,16 @@
-const how_sum=(target,nums,memo={})=>{
-if(memo[target] in memo) return memo[target]
-if(target==0) return []
-if(target<0) return null
-for(let num of nums){
-var remaining= target- num
-var remainingResult=how_sum(remaining,nums, memo)
-if(remainingResult!=null){
-memo[target]=[... remainingResult,num]
-return memo[target]
+const how_sum = (target, nums, memo = {}) => {
+  if (memo[target] in memo) return memo[target]
+  if (target === 0) return []
+  if (target < 0) return null
+  for (let num of nums) {
+    var remaining = target - num
+    var remainingResult = how_sum(remaining, nums, memo)
+    if (remainingResult != null) {
+      memo[target] = [...remainingResult, num]
+      return memo[target]
+    }
+  }
+  memo[target] = null
+  return memo[target]
 }
-}
-memo[target]=null
-return memo [target]
-}
+console.log(how_sum(7, [2, 3]))
