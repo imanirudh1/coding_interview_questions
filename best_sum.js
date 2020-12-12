822d336780ad8342e384f9fdc22ec1b46899669f
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-12 20:25:38 +0530

    Best Sum using tabulation

--- best_sum.js
+++ best_sum.js
@@ -1,23 +1,43 @@
-const best_sum = (target, nums, memo = {}) => {
-  if (memo[target] in memo) return memo[target]
-  if (target === 0) return []
-  if (target < 0) return null
-  var shortestCombination = null
-  for (let num of nums) {
-    var remainder = target - num
-    var remainderCombination = best_sum(remainder, nums, memo)
-    if (remainderCombination != null) {
-      var combination = [...remainderCombination, num]
-      if (
-        shortestCombination === null ||
-        combination.length < shortestCombination.length
-      ) {
-        shortestCombination = combination
+// const best_sum = (target, nums, memo = {}) => {
+//   if (memo[target] in memo) return memo[target]
+//   if (target === 0) return []
+//   if (target < 0) return null
+//   var shortestCombination = null
+//   for (let num of nums) {
+//     var remainder = target - num
+//     var remainderCombination = best_sum(remainder, nums, memo)
+//     if (remainderCombination != null) {
+//       var combination = [...remainderCombination, num]
+//       if (
+//         shortestCombination === null ||
+//         combination.length < shortestCombination.length
+//       ) {
+//         shortestCombination = combination
+//       }
+//     }
+//   }
+//   memo[target] = shortestCombination
+//   return memo[target]
+// }
+
+const best_sum = (target, nums) => {
+  const table = Array(target + 1).fill(null)
+  table[0] = []
+  for (let i = 0; i <= target; i++) {
+    if (table[i] != null) {
+      for (let num of nums) {
+        if (num + i <= target) {
+          let arr = [...table[i], num]
+          if (table[i + num] === null) {
+            table[i + num] = arr
+          } else if (table[i + num] && arr.length < table[i + num].length) {
+            table[i + num] = arr
+          }
+        }
       }
     }
   }
-  memo[target] = shortestCombination
-  return memo[target]
+  return table[target]
 }
 
-console.log(best_sum(100, [34, 36, 2, 25]))
+console.log(best_sum(100, [1, 2, 3, 5, 25]))
