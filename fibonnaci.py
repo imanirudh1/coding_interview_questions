0df7917d5c23bb69a5f5f5be77ba8ffa7729f877
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-12 18:59:28 +0530

    Fibonnaci series using Tabulation

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,51 +1,62 @@
-a=10 
-def fibonnaci(a):
-    a,b=0,1
-    for i in range(n):
-        a,b=b,a+b
-    return a
+# a=10 
+# def fibonnaci(a):
+#     a,b=0,1
+#     for i in range(n):
+#         a,b=b,a+b
+#     return a
 
-def rec_fibo(a):
-    if a==0 or a==1:
-        return a
-    return fibonnaci(a-1)+fibonnaci(a-2) 
+# def rec_fibo(a):
+#     if a==0 or a==1:
+#         return a
+#     return fibonnaci(a-1)+fibonnaci(a-2) 
 
 
-import sys 
-sys.setrecursionlimit(10000)
-cache={}
-def memo_fibo(a):
-    if a in cache:
-        return cache[a]
+# import sys 
+# sys.setrecursionlimit(10000)
+# cache={}
+# def memo_fibo(a):
+#     if a in cache:
+#         return cache[a]
 
-    if a ==1 or a==2:
-        value=1
-    elif a>2:
-        value=memo_fibo(a-1)+memo_fibo(a-2)
-    cache[a]=value
-    return value                
+#     if a ==1 or a==2:
+#         value=1
+#     elif a>2:
+#         value=memo_fibo(a-1)+memo_fibo(a-2)
+#     cache[a]=value
+#     return value                
 
-# print(memo_fibo(100))
+# # print(memo_fibo(100))
 
-# using Memoization
-from functools import lru_cache
+# # using Memoization
+# from functools import lru_cache
 
-@lru_cache(maxsize=10000)
-def memo_fibo_lru(a):
-    if a ==1 or a==2:
-       return 1
-    elif a>2:
-        return memo_fibo(a-1)+memo_fibo(a-2)
-print(memo_fibo_lru(1000))    
+# @lru_cache(maxsize=10000)
+# def memo_fibo_lru(a):
+#     if a ==1 or a==2:
+#        return 1
+#     elif a>2:
+#         return memo_fibo(a-1)+memo_fibo(a-2)
+# print(memo_fibo_lru(1000))    
 
 
 
 
 #Memonization
-def fibo(n,memo={}):
-    if n in memo:
-        return memo[n]
-    if n <=2:
-        return 1
-    memo[n]=fibo(n-1,memo) + fibo(n-2,memo)
-    return memo[n]
+# def fibo(n,memo={}):
+#     if n in memo:
+#         return memo[n]
+#     if n <=1:
+#         return n
+#     memo[n]=fibo(n-1,memo) + fibo(n-2,memo)
+#     return memo[n]
+# print(fibo(2))
+
+def fibo(n):
+    table = [0 for i in range(n + 1)]
+    table[1] = 1
+    for i in range(n+1):
+        if i<n : table[i+1]+=table[i]
+        if i<n-1 : table[i+2]+=table[i]
+    return table[n]
+
+print(fibo(50))
