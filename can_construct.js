0d5d7d18d2a0b16e98ddf6881981577fdc0eb821
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-13 18:49:58 +0530

    Can Construct using tabulation

--- can_construct.js
+++ can_construct.js
@@ -1,17 +1,32 @@
-const can_construct = (target, wordbank, memo = {}) => {
-  if (memo[target] in memo) return memo[target]
-  if (target === '') return true
-  for (let word of wordbank) {
-    if (target.indexOf(word) === 0) {
-      let suffix = target.slice(word.length)
-      if (can_construct(suffix, wordbank, memo)) {
-        memo[target] = true
-        return true
+// const can_construct = (target, wordbank, memo = {}) => {
+//   if (memo[target] in memo) return memo[target]
+//   if (target === '') return true
+//   for (let word of wordbank) {
+//     if (target.indexOf(word) === 0) {
+//       let suffix = target.slice(word.length)
+//       if (can_construct(suffix, wordbank, memo)) {
+//         memo[target] = true
+//         return true
+//       }
+//     }
+//   }
+//   memo[target] = false
+//   return false
+// }
+
+const can_construct = (target, wordbank) => {
+  const table = Array(target.length + 1).fill(false)
+  table[0] = true
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i] === true) {
+      for (let word of wordbank) {
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true
+        }
       }
     }
   }
-  memo[target] = false
-  return false
+  return table[target.length]
 }
 
 console.log(can_construct('abcdef', ['abc', 'ab', 'cd', 'def', 'abcd']))
