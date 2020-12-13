1e1a0b6f846e7076a9853e7d9c1915ff7547d181
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-13 20:13:16 +0530

    All Construct using tabulation

--- allConstruct.js
+++ allConstruct.js
@@ -1,21 +1,41 @@
-const allConstruct = (target, wordbank, memo = {}) => {
-  if (memo[target] in memo) return memo[target]
-  if (target === '') return [[]]
-  var results = []
+// const allConstruct = (target, wordbank, memo = {}) => {
+//   if (memo[target] in memo) return memo[target]
+//   if (target === '') return [[]]
+//   var results = []
 
-  for (let word of wordbank) {
-    if (target.indexOf(word) === 0) {
-      let suffix = target.slice(word.length)
-      const possible = allConstruct(suffix, wordbank, memo)
-      const targetWays = possible.map((ways) => [word, ...ways])
-      results.push(...targetWays)
+//   for (let word of wordbank) {
+//     if (target.indexOf(word) === 0) {
+//       let suffix = target.slice(word.length)
+//       const possible = allConstruct(suffix, wordbank, memo)
+//       const targetWays = possible.map((ways) => [word, ...ways])
+//       results.push(...targetWays)
+//     }
+//   }
+//   memo[target] = results
+//   return results
+// }
+
+const allConstruct = (target, wordbank) => {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => [])
+  table[0] = [[]]
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i].length > 0) {
+      for (let word of wordbank) {
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length].push(
+            ...table[i].map((item) => [...item, word])
+          )
+        }
+      }
     }
   }
-  memo[target] = results
-  return results
+  return table[target.length]
 }
+
 console.log(
-  allConstruct('abcdef', ['abc', 'ab', 'cd', 'def', 'abcd', 'abcdef'])
+  allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c'])
 )
 console.log(
   allConstruct('eeeeeeeeeeeeeeeeeeeeeeef', ['fe', 'feeeef', 'eeeeeeef'])
